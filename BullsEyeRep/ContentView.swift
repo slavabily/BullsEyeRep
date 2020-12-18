130ee7e3cb8fe1d9009902009d512144bdb19fd5
slava bily <slavabily@me.com>
2020-12-18 22:23:45 +0200

    static UI is crated

--- BullsEyeRep/ContentView.swift
+++ BullsEyeRep/ContentView.swift
@@ -8,9 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var targetGuess = " "
+    @State private var sliderValue = 50.0
+    @State private var selectedGameStyle = 0
+    @State private var round = 0
+    @State private var score = 0
+    
+    var gameStyles = ["Slide", "Type"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    HStack {
+                        Text("Get close or guess where:")
+                        TextField("", text: $targetGuess)
+                    }
+                    Slider(value: $sliderValue, in: 1...100, minimumValueLabel: Text("1"), maximumValueLabel: Text("100")) {
+                        
+                    }
+                }
+                Button("Hit Me!") {
+                    
+                }
+                .frame(width: 300, height: 0, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                Section {
+                    Button("Start over") {
+                        
+                    }
+                    .frame(width: 300, height: 0, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                }
+                Section {
+                    Picker(" ", selection: $selectedGameStyle) {
+                        ForEach(0..<gameStyles.count) {
+                            Text(gameStyles[$0])
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                Section {
+                    
+                    Text("Round: \(round)")
+                    
+                    Text("Score: \(score)")
+                    
+                }
+                .frame(width: 300, height: 0, alignment: .center)
+            }
+            .navigationBarTitle(Text("BullsEyeRep"))
+            
+        }
+        
     }
 }
 
--- BullsEyeRepTests/BullsEyeRepTests.swift
+++ BullsEyeRepTests/BullsEyeRepTests.swift
@@ -9,25 +9,40 @@ import XCTest
 @testable import BullsEyeRep
 
 class BullsEyeRepTests: XCTestCase {
+    
+    var sut: BullsEyeGame!
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+        sut = BullsEyeGame()
+        sut.startNewGame()
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = nil
+        super.tearDown()
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testScoreIsComputed() {
+        // given
+        let guess = sut.targetValue + 5
+        
+        // when
+        sut.check(guess: guess)
+        
+        // then
+        XCTAssertEqual(sut.scoreRound, 95, "Scorecomputed from guess is wrong")
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testScoreIsComputedWhenGuessLTTarget() {
+      // 1. given
+      let guess = sut.targetValue - 5
+      // 2. when
+      sut.check(guess: guess)
+      // 3. then
+      XCTAssertEqual(sut.scoreRound, 95, "Score computed from guess is wrong")
     }
 
 }
